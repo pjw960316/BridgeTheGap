02c9901d448697d48b69deae5d16bbd5774eee26
pjw960316 <pjw960316@naver.com>
2022-04-15 14:37:45 +0900

    04/15 : start

---
+++ Practice With Code/C Shart Practice/C Shart Practice/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace C_Shart_Practice
+{
+    class Class1
+    {
+        Thread a = new Thread(1);
+    }
+}
