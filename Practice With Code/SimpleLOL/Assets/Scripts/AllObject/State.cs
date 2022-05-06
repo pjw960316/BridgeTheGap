61b384aa33515e65d4e33f4987c18ed8d7814bcd
pjw960316 <pjw960316@naver.com>
2022-05-06 14:30:53 +0900

    05/06 : 난관에 봉착 - 지금까지 배운 내용을 적용해서 간단한 LOL Simulation을 구현해보고 싶다. - 이전에 패턴없이 돌아가도록만 구현한다면 하루면 할 것 같다. - 하지만 패턴+객체지향에 대해 고려해보고 유지보수를 생각해보니 한줄도   제대로 구현하지 못하겠다. 공부가 더 필요하다. - 디자인패턴 + 객체지향

---
+++ Practice With Code/SimpleLOL/Assets/Scripts/AllObject/State.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State 
+{
+    public int ad { get; private set; }
+
+    public State(int a)
+    {
+        ad = a;
+    }
+}
